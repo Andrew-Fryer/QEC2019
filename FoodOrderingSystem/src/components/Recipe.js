a9a3b12071b571a39ce522745ce284483e08d2f3
afryer <andrewtfryer@gmail.com>
2019-10-05 10:50:51 -0400

    wip recipe component

---
+++ FoodOrderingSystem/src/components/Recipe.js
@@ -0,0 +1,13 @@
+import React from 'react';
+class Recipe extends React.Component {
+  render() {
+    return (
+      <div className="Recipe">
+        Here 'tis
+        
+      </div>
+    );
+  }
+}
+
+export default Recipe;
