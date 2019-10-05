0574fe7d2ec45c1d8e9e22d5d9b2e33b6558b78f
ksinger12 <54747036+ksinger12@users.noreply.github.com>
2019-10-05 14:32:46 -0400

    Update index.js

--- FoodOrderingSystem/src/pages/index.js
+++ FoodOrderingSystem/src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 import Login from "../components/login"
 
@@ -19,16 +18,13 @@ const IndexPage = () => (
         </p>
       </div>
       <Login />
+      <Link to="/Account/">Create Account</Link>
       <br/>
-      <br/>
-    </div>
-
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      {/*<Image />*/}
+      
     </div>
     <Link to="/FoodInput/">Go to page 2</Link>
     <br/>
-    <Link to="/Account/">Create Account</Link>
+    
   </Layout>
 )
 
